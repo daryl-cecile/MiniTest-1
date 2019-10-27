4ad9c9cded4474b6e9bfe6e7e96e5fda721d9a91
Daryl <darylcecile@gmail.com>
2019-10-27 20:28:55 +0000

    updated solution
    readded the splicer

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 
 function checkAnagram(wordA,wordB){
+
     wordA = wordA.replace(/ /g,'');
     wordB = wordB.replace(/ /g,'');
 
     if (wordA === wordB) return false;
     if (wordA.length !== wordB.length) return false;
     
+    let wordALetters = wordA.split('');
+    
     for(let i = 0; i < wordB.length; i++){
-        if ( wordA.indexOf(wordB[i]) === -1 ){
-            return false;
+
+        if ( wordALetters.indexOf(wordB[i]) > -1 ){
+            wordALetters.splice( wordA.indexOf(wordB[i]) , 1 );
         }
+        else return false
+
     }
     return true;
 
